487ee8f74ce5ae2f57d7e5fe084b763373815817
ndaboom <ndabosam084@gmail.com>
2022-12-09 00:21:18 +0100

    App UI updated & Crud operations to tasks done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,68 @@
 import React from "react";
+import styles from "./TodoItem.module.css";
 
 class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  };
   render() {
-    return <li><input type="checkbox" checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)} /> {this.props.todo.title} <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button></li>
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    };
+    const { completed, id, title } = this.props.todo;
+
+    const handleEditing = () => {
+      handleEditing = () => {
+        this.setState({
+          editing: true,
+        });
+      }
+    }
+
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+
+    handleUpdatedDone = event => {
+      if (event.key === "Enter") {
+        this.setState({ editing: false })
+      }
+    }
+
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={e => {
+            this.props.setUpdate(e.target.value, id)
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
+        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+        </div>
+        <input
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={title}
+          onChange={e => {
+            console.log(e.target.value, id)
+          }}
+        />
+      </li>
+    )
   }
 }
 
